70ef8e42b29c251c099c079360f422d375118abe
Felix Nguyen <ackar8985@gmail.com>
2023-02-11 16:48:34 -0800

    added comments

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 print("Hello World!!! :D")
+
+#git status: check status of repository
+#git log: 
+#git add: Modified -> Staged
+#git commit: Staged -> Commited
+#git commit -m: Staged -> Commited (with quick message)
+#git commit --amend: Commit on top of most recent commit
+#git commit --amend --no-edit: Commit on top of most recent commit with no new message
+#git stash: save file temporarily without commit
