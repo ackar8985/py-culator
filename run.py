b8d433d1d0e2cc3146cce9f8519893c48d4f23f4
Felix Nguyen <ackar8985@gmail.com>
2023-02-11 17:18:36 -0800

    added add function

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 print("Hello World!!! :D")
 
+def add(a, b):
+    return a+b
+
+print(add(1,2))
+
 #git status: check status of repository
 #git log: 
-#git add: Modified -> Staged
+#git add [filename]: Modified -> Staged
 #git commit: Staged -> Commited
 #git commit -m: Staged -> Commited (with quick message)
 #git commit --amend: Commit on top of most recent commit
